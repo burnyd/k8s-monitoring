5e5cbc1b1f502f55a5e0550c20dd6a5a6c55b705
dan <burnyd@gmail.com>
2021-03-16 01:11:18 +0000

    made changes

--- influx/install.sh
+++ influx/install.sh
@@ -1,5 +1,5 @@
 kubectl create secret generic influxdb-creds -n monitoring \
   --from-literal=INFLUXDB_DATABASE=local_monitoring \
-  --from-literal=INFLUXDB_USERNAME=root \
-  --from-literal=INFLUXDB_PASSWORD=password \
+  --from-literal=INFLUXDB_USERNAME=admin \
+  --from-literal=INFLUXDB_PASSWORD=admin \
   --from-literal=INFLUXDB_HOST=influxdb
