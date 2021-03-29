a29ff4b076d83c22944f016f0601036ac96ba0e6
dan <burnyd@gmail.com>
2021-03-29 19:58:46 +0000

    added batch jobs for grafana export.

--- grafana/install.sh
+++ grafana/install.sh
@@ -1,3 +1,5 @@
 kubectl create secret generic grafana-creds -n monitoring \
   --from-literal=GF_SECURITY_ADMIN_USER=admin \
   --from-literal=GF_SECURITY_ADMIN_PASSWORD=admin
+
+  kubectl create configmap grafana-export-cm --from-file=export.py -n monitoring 
